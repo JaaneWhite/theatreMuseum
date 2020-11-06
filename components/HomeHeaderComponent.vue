1dc44ef5acd07ca9263f8003cd0907ca88fdfb7e
Anastasia <a.shironosova@altsoft.spb.ru>
2020-11-06 18:19:02 +0300

    +header

--- components/HomeHeaderComponent.vue
+++ components/HomeHeaderComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <header>
-  <b-row class="header-carousel">
-    <b-col class="header-left">
+    <b-row class="header-carousel-row">
+      <b-col class="header-left">
 
-    </b-col>
-    <b-col class="header-right">
-      <b-carousel
+      </b-col>
+      <b-col class="header-right">
+      <b-carousel class="header-carousel"
           id="carousel-1"
           v-model="slide"
           :interval="4000"
@@ -22,7 +22,22 @@
 
       </b-carousel>
     </b-col>
-  </b-row>
+
+
+    </b-row>
+    <div class="header-title-block">
+      <b-container >
+        <b-col>
+          <b-row class="project-text"></b-row>
+          <b-row class="title-text">ТЕАТРАЛЬНЫЕ МУЗЕИ И АРХИВЫ РОССИИ И РУССКОГО ЗАРУБЕЖЬЯ</b-row>
+
+        </b-col>
+        <b-col>
+
+        </b-col>
+      </b-container>
+    </div>
+
   <b-row class="menu-row">
     <b-container class="menu-row">
       <TopMenu/>
@@ -41,18 +56,37 @@ export default {
 </script>
 
 <style scoped>
-.header-carousel {
+.header-carousel-row {
+
   background-image: url('~assets/img/header bg.png');
   height: 475px;
+  z-index: 0;
 }
 .header-right {
   display: flex;
   text-align: right;
 }
+.header-carousel {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
 .slide-img {
   height: 475px;
   width: 980px;
 }
+.header-title-block {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: 10;
+  border: solid black 1px;
 
+}
+.title-text {
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+}
 
 </style>
