d533ccdf1fe801569cde7f0f7d52b3de1a7f6ee9
Anastasia <a.shironosova@altsoft.spb.ru>
2020-11-09 16:46:50 +0300

    +пагинация

---
+++ components/Pagination.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-row class="pagination">
+    <b-pagination
+        v-model="currentPage"
+        pills
+        :total-rows="rows"
+        :per-page="perPage"
+        first-number
+        last-number
+
+    ></b-pagination>
+  </b-row>
+</template>
+<script>
+export default {
+  name: 'pagination',
+  data() {
+    return {
+      rows: 100,
+      currentPage: 1
+    }
+  }
+}
+</script>
+<style scoped>
+.page-item.disabled .page-link, .page-link, .page-item:hover .page-link {
+  color: black;
+  background-color: transparent;
+  border-color: transparent;
+  font-size: 16px;
+  font-family: Roboto;
+}
+.page-item.active .page-link {
+  color: white;
+  background-color: #aad1c9;
+  border: none;
+  font-size: 16px;
+  font-family: Roboto;
+}
+</style>
--- pages/collections.vue
+++ pages/collections.vue
@@ -1,4 +1,40 @@
 <template>
+  <b-container class="main-content">
+    <b-row class="breadcrumb-row">
+      <b-breadcrumb :items="items"></b-breadcrumb>
+    </b-row>
+    <b-row class="page-title">Экспонаты</b-row>
+    <b-row>
+      <b-col cols="3" class="menu-col">
+        <b-nav vertical class="collection-menu">
+          <b-nav-item to="#afi" exact exact-active-class="active" class="c-menu-button">Афиши</b-nav-item>
+          <b-nav-item class="c-menu-button">Декорации</b-nav-item>
+          <b-nav-item class="c-menu-button">Документы, рукописи</b-nav-item>
+          <b-nav-item class="c-menu-button">Книги</b-nav-item>
+          <b-nav-item class="c-menu-button">Костюмы, грим</b-nav-item>
+          <b-nav-item class="c-menu-button">Куклы</b-nav-item>
+          <b-nav-item class="c-menu-button">Награды</b-nav-item>
+          <b-nav-item class="c-menu-button">Портреты</b-nav-item>
+          <b-nav-item class="c-menu-button">Программы</b-nav-item>
+          <b-nav-item class="c-menu-button">Реквизит, бутафория, предметы</b-nav-item>
+          <b-nav-item class="c-menu-button">Скульптура</b-nav-item>
+          <b-nav-item class="c-menu-button">Стенды</b-nav-item>
+          <b-nav-item class="c-menu-button">Сцены из спектаклей</b-nav-item>
+        </b-nav>
+      </b-col>
+      <b-col cols="9">
+        <b-row>
+          <b-col cols="6" class="collection-title">Афиши</b-col>
+          <b-col cols="6" class="collection-num">Показано экспонатов: 1-12 из 774</b-col>
+        </b-row>
+        <b-row class="pagination-row">
+          <pagination/>
+
+        </b-row>
+
+      </b-col>
+    </b-row>
+  </b-container>
 
 
 
@@ -6,12 +42,44 @@
 </template>
 
 <script>
+import Pagination from "~/components/Pagination";
 export default {
   name: "collections",
-  layout: "default"
+  components: {Pagination},
+  layout: "default",
+  data() {
+    return {
+      items: [
+        {
+          text: 'Главная',
+          to: '/'
+        },
+        {
+          text: 'Коллекции',
+          active: true
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.menu-col {
+  border-right: solid #270000 1px;
+  padding: 0;
+}
+.c-menu-button a {
+  padding-left: 0;
+}
+.collection-title {
+  font-size: 1vw;
+  font-weight: bold;
+}
+.collection-num {
+  text-align: right;
+}
+.pagination-row {
+  text-align: center;
+}
 </style>
